a8d2ae363f7a030d2fa3ed8ff78b67507c0ea6c7
Fernando Leme <fernando.silva824@etec.sp.gov.br>
2022-08-16 08:54:42 -0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
+
+const routes: Routes = [
+  {
+    path: '', pathMatch: 'full', redirectTo: 'cursos'
+  },
+  {
+    path: 'cursos',
+    loadChildren: ()=> import('./cursos/cursos.module').then(m => m.CursosModule)
+  },
+  {
+    path: 'alunos',
+    loadChildren: ()=> import('./alunos/alunos.module').then(m => m.AlunosModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
